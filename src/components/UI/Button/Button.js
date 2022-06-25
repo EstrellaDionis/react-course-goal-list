a584097278fb35ce134a4de703d8a40297352812
EstrellaDionis <EstrellaDionis@gmail.com>
2022-06-25 13:02:16 -0400

    incorporated media queries on button

--- src/components/UI/Button/Button.js
+++ src/components/UI/Button/Button.js
@@ -3,6 +3,7 @@ import styled from 'styled-components';
 //.button is a method from styled package
 const Button = styled.button`
 //this will be the default button style
+  width: 100%
   font: inherit;
   padding: 0.5rem 1.5rem;
   border: 1px solid #8b005d;
@@ -11,14 +12,18 @@ const Button = styled.button`
   box-shadow: 0 0 4px rgba(0, 0, 0, 0.26);
   cursor: pointer;
 
+  @media (min-width: 768px){
+    width: auto;
+  }
+
 //instead of needing .button:focus... we just do &:focus 
-&:focus {
+  &:focus {
   outline: none;
 }
 
 //instead of .button:hover xx &:hover and etc etc
-&:hover,
-&:active {
+  &:hover,
+  &:active {
   background: #ac0e77;
   border-color: #ac0e77;
   box-shadow: 0 0 8px rgba(0, 0, 0, 0.26);
