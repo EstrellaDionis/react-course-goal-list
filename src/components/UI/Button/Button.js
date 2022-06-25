467ab4b0daae10633003796b4c799046b003c8b2
EstrellaDionis <EstrellaDionis@gmail.com>
2022-06-25 13:15:50 -0400

    reverted button css files and using css modules to make the button useable

--- src/components/UI/Button/Button.js
+++ src/components/UI/Button/Button.js
@@ -1,41 +1,48 @@
-import styled from 'styled-components';
+import React from 'react'
+
+// import styled from 'styled-components';
+
+//note how it is now Button.module.css. We need this to activate CSS modules and do what we're doing
+import styles from './Button.module.css'
 
 //.button is a method from styled package
-const Button = styled.button`
-//this will be the default button style
-  width: 100%
-  font: inherit;
-  padding: 0.5rem 1.5rem;
-  border: 1px solid #8b005d;
-  color: white;
-  background: #8b005d;
-  box-shadow: 0 0 4px rgba(0, 0, 0, 0.26);
-  cursor: pointer;
+// const Button = styled.button`
+// //this will be the default button style
+//   width: 100%
+//   font: inherit;
+//   padding: 0.5rem 1.5rem;
+//   border: 1px solid #8b005d;
+//   color: white;
+//   background: #8b005d;
+//   box-shadow: 0 0 4px rgba(0, 0, 0, 0.26);
+//   cursor: pointer;
 
-  @media (min-width: 768px){
-    width: auto;
-  }
+//   @media (min-width: 768px){
+//     width: auto;
+//   }
 
-//instead of needing .button:focus... we just do &:focus 
-  &:focus {
-  outline: none;
-}
+// //instead of needing .button:focus... we just do &:focus 
+//   &:focus {
+//   outline: none;
+// }
 
-//instead of .button:hover xx &:hover and etc etc
-  &:hover,
-  &:active {
-  background: #ac0e77;
-  border-color: #ac0e77;
-  box-shadow: 0 0 8px rgba(0, 0, 0, 0.26);
-}
-`;
+// //instead of .button:hover xx &:hover and etc etc
+//   &:hover,
+//   &:active {
+//   background: #ac0e77;
+//   border-color: #ac0e77;
+//   box-shadow: 0 0 8px rgba(0, 0, 0, 0.26);
+// }
+// `;
 
-// const Button = props => {
-//   return (
-//     <button type={props.type} className="button" onClick={props.onClick}>
-//       {props.children}
-//     </button>
-//   );
-// };
+
+//styles.button comes from our naming "styles" and the .button from the css sheet
+const Button = props => {
+  return (
+    <button type={props.type} className={styles.button} onClick={props.onClick}>
+      {props.children}
+    </button>
+  );
+};
 
 export default Button;
